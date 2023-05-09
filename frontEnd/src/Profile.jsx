329bf123b915b57f1603929013639aa6d70b1279
Sabrola <70954761+Sabrola@users.noreply.github.com>
2023-05-09 23:29:36 +0000

    [09/05] brigando com card

--- frontEnd/src/Profile.jsx
+++ frontEnd/src/Profile.jsx
@@ -6,29 +6,17 @@ import '../styles/index.css';
 import '../styles/profile.css';
 import '../styles/utils.css';
 
-import Tarefas from './Tarefas/cards.jsx';
-import contents from './contents.jsx';
+import contents from './Tarefas/cards.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <div className='tarefa'>
-        {contents.map(contents => (
-            <Tarefas
-                key={contents.id}
-                nome={contents.notif_nome}
-                data={contents.data}
-                hora={contents.hora}
-                desc={contents.conteudo}
-                notif={contents.notif_em}
-                repete={contents.tempo_id}
-            />
-        ))}
-        </div>
-    </React.StrictMode>,
-  )
-
-export default function profile(){
-    return( 
-        <Tarefas />
-    )
+function cards_contentstarefas(Props) {
+    const cards_info = contents.map((contents) =>
+    <li> key={contents.id}</li>
+    );
+    return (
+        <ul> {cards_info} </ul>
+    );
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render( <NumberList numbers={numbers} />);
+
--- frontEnd/src/contents.jsx
+++ frontEnd/src/contents.jsx
@@ -1,10 +1,11 @@
-
 const contents = [
-    id, 0o3,
-    notif_nome, "Calrs",
-    data, 0o4/0o5,
-    hora, "13.30",
-    conteudo, "hgfdsdfghnjhgfdsdfghnasdasdasdasdasfsdsdffdbdvsfasfgsdfdbgvf",
-    notif_em, "3 dias",
-    tempo_id, "15 dias",
-]
+    id = 1,
+    notif_nome = "Calrs",
+    data = "04/05",
+    hora = "13.30",
+    conteudo = "hgfdsdfghnjhgfdsdfghnasdasdasdasdasfsdsdffdbdvsfasfgsdfdbgvf",
+    notif_em = "3 dias",
+    tempo_id = "15 dias" ]
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(contents);
