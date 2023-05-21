4792c214b852d01534ebc08c1327a39f4fe94d88
Sabrola <70954761+Sabrola@users.noreply.github.com>
2023-05-21 23:26:59 +0000

    [21/05] Cards

--- frontEnd/src/Tarefas/cards.jsx
+++ frontEnd/src/Tarefas/cards.jsx
@@ -11,33 +11,32 @@ export default function Lembretes() {
 
                 {/* Titulo */} 
                 <div className='display_lembretes_top'>
-                    <h2 className='title'>{card.notif_name}</h2>
                     <div className='tarefa_date'>{card.data}
                         <section className='tarefa_hora'> {card.hora}</section>
                     </div>
+                    <h2 className='title'>{card.notif_name}</h2>
                 </div>
                 {/* Titulo */}
 
                 {/* Conteudo */}
+                <div className="content">
+                    <article className='lembretes_desc'>
+                            {' ' + card.content + ' '}
+                    </article>
+                </div>
+
                 <div className='display_tarefas_content'>
 
                     {/* Esquerda */}
                     <div className='content_left'>
-                        <article className='lembretes_desc'>
-                            {' ' + card.content + ' '}
-                        </article>
 
-                        <article className='lembretes_btns'>
-                            <button className="lembretes_btn edit"> Editar</button>
-                            <button className='lembretes_btn delet'> Deletar</button>
-                        </article>
+                        <section className='notifs'>Notificação: {card.notif_em}</section>
                     </div>
                     {/* Esquerda */}
 
                     {/* Direita */}
                     <div className='content_right'>
-                        <section className='notifs'>Notificação: {card.notif_em}</section>
-                        <hr />
+
                         <section className='repeat'> Repetindo : {card.repetir}</section>
                     </div>
                     {/* Direita */}
--- frontEnd/src/contents.jsx
+++ frontEnd/src/contents.jsx
@@ -2,24 +2,40 @@ export const notifs = [{
     notif_id: 0,
     notif_name: 'nome_01',
     content: 'rapapapapa, papappapa, prpapapapa1',
-    data: '05/08',
+    data: '05/08/0000',
     hora: '15:30',
-    notif_em: 'sim',
+    notif_em: '6',
     repetir: '5'
 }, {
     notif_id: 1,
     notif_name: 'nome_02 looooooooooooooooo oong',
     content: 'rapapapapa, papappapa, prpapapapa2',
-    data: '28/06',
+    data: '28/06/0000',
     hora: '13:00',
-    notif_em: 'sim',
+    notif_em: '8',
     repetir: '3'
 }, {
     notif_id: 2,
     notif_name: 'nome_03 super duper uper long long',
     content: 'rapapapapa, papappapa, prpapapapa3 super long wong wonf wong wong sonasoao oasnd ojad ojda',
-    data: '20/11',
+    data: '20/11/2023',
     hora: '12:00',
-    notif_em: 'n',
+    notif_em: '1',
     repetir: '15'
+}, {
+    notif_id: 3,
+    notif_name: 'nome_04  uper long long',
+    content: 'rapapapapa, papappapa, prpapapapa3 super long wog wonf wong wong sonasoao oasnd ojad ojda',
+    data: '34/12/0000',
+    hora: '12:50',
+    notif_em: '',
+    repetir: '35'
+}, {
+    notif_id: 4,
+    notif_name: 'nome_05 super duper upeg',
+    content: 'rapapapapa, papapad ojda',
+    data: '09/04/0000',
+    hora: '16:00',
+    notif_em: '',
+    repetir: ''
 }];
