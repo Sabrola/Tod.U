1975c7dc3bdf5e494e3b24f725ccde7ffd63ed8c
Sabrola <70954761+Sabrola@users.noreply.github.com>
2023-06-08 22:52:43 +0000

    [08/06] Login começo

--- frontEnd/src/Login_app.jsx
+++ frontEnd/src/Login_app.jsx
@@ -1,51 +1,37 @@
 { /*////////////////// App.jsx ///////////////*/ }
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import '../styles/modern-normalize.css';
 import '../styles/index.css';
 import '../styles/Login.css';
 import '../styles/utils.css';
 
-import usuarios from './Stuff/users'
 
 function Login_stuff() {
+    const [username, setUsername] = useState("");
+    const [senha, setSenha] = useState("");
     
-    const [users, setUsers] = useState(usuarios);
-
-    const [loginState, ] = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-
-        const fieldName = e.target.getAttribute(users.user_id)
-    }
-
-    const validateLogin = (e) => {
-        if(loginState.user && loginState.description) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    const handleSubmit = (newLogin) => {
-        setUsers([...users, newLogin])
-
-        if (!validateLogin()) return;
+        console.log("submit", { username, senha});
     }
 
     return (
         <div>
         
-        <form>       
+        <form onSubmit={handleSubmit}>       
             <div className="textfield">
-                <label for="user">Usuário:</label>
-                <input id="user" name="nickname" type="text" placeholder="Usuário"></input>
+                <label htmlFor="user">Usuário:</label>
+                <input name="nickname" type="text" placeholder="Usuário"
+                    id="user" value={username} onChange={(e) => setUsername(e.target.value)}></input>
             </div>
             <div className="textfield">
-                <label for="senha">Senha:</label>
-                <input id="senha" name="password" type="password" placeholder="Senha"></input>
+                <label htmlFor="senha">Senha:</label>
+                <input name="password" type="password" placeholder="Senha"
+                    id="senha" value={senha} onChange={(e) => setSenha(e.target.value)}></input>
             </div>
     
-            <button className="btn-next" type="submit" onClick={handleSubmit}>Próximo</button>
+            <button className="btn-next" type="submit">Próximo</button>
         </form>
     </div>
     )
